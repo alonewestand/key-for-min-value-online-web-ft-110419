6daff6f3fbe57a9314e0ca6b062c59c8fd96ba24
Ken Goad <goadke@g.cofc.edu>
2019-11-09 00:39:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@ def key_for_min_value(hash)
   small_key = nil
   small_value = nil
   hash.each do |key, value|
-    if small_value == nil || v < small_value
+    if small_value == nil || value < small_value
       small_value = value
       small_key = key
     end
